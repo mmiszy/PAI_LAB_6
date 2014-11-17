621c37a3011558151698a7a2d803b7cfcbe88e61
Kuba Jagoda <kuba.jagoda+github@gmail.com>
2014-11-17 20:54:38 +0100

    Added data and basic model class

---
+++ Server/Model.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Server
+{
+    public class Model
+    {
+        private uint Id { get; set; }
+        private String Name {get; set;}
+        private uint Age { get; set; }
+        private String City { get; set; }
+        private String Address { get; set; }
+    }
+}
