8a1649f75b84c302ade30a0e1c6a22726ce2d43b
Kuba Jagoda <kuba.jagoda+github@gmail.com>
2014-11-18 00:35:37 +0100

    Added previous and next buttons in client

--- Client/Form1.Designer.cs
+++ Client/Form1.Designer.cs
@@ -30,12 +30,14 @@
         {
             this.components = new System.ComponentModel.Container();
             this.PersonsList = new System.Windows.Forms.DataGridView();
-            this.modelBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ageDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.cityDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.addressDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.modelBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.PersonsList)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.modelBindingSource)).BeginInit();
             this.SuspendLayout();
@@ -55,10 +57,6 @@
             this.PersonsList.Name = "PersonsList";
             this.PersonsList.Size = new System.Drawing.Size(547, 237);
             this.PersonsList.TabIndex = 0;
-            // 
-            // modelBindingSource
-            // 
-            this.modelBindingSource.DataSource = typeof(Client.Server.Model);
             // 
             // idDataGridViewTextBoxColumn
             // 
@@ -90,11 +88,37 @@
             this.addressDataGridViewTextBoxColumn.HeaderText = "Address";
             this.addressDataGridViewTextBoxColumn.Name = "addressDataGridViewTextBoxColumn";
             // 
+            // modelBindingSource
+            // 
+            this.modelBindingSource.DataSource = typeof(Client.Server.Model);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "« Previous";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 255);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Next »";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(630, 344);
+            this.ClientSize = new System.Drawing.Size(601, 309);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.PersonsList);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -113,6 +137,8 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn cityDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn addressDataGridViewTextBoxColumn;
         private System.Windows.Forms.BindingSource modelBindingSource;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Client/Form1.cs
+++ Client/Form1.cs
@@ -13,16 +13,47 @@ namespace Client
     public partial class Form1 : Form
     {
         private const int DATA_SLICE = 10;
+        private int RecordsFrom = 0;
 
         public Form1()
         {
             InitializeComponent();
+            this.ReloadData();            
+        }
 
+        private void ReloadData()
+        {
             Server.Server server = new Server.Server();
-            List<Server.Model> persons = server.GetData(0, Form1.DATA_SLICE).ToList();
+            List<Server.Model> persons = server.GetData(this.RecordsFrom, Form1.DATA_SLICE).ToList();
 
             BindingList<Server.Model> personsList = new BindingList<Server.Model>(persons);
             this.PersonsList.DataSource = personsList;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.RecordsFrom -= Form1.DATA_SLICE;
+            if (this.RecordsFrom < 0)
+            {
+                this.RecordsFrom = 0;
+            } 
+            else
+            {
+                this.ReloadData();
+            }            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.RecordsFrom += Form1.DATA_SLICE;
+            BindingList<Server.Model> personsList = (BindingList<Server.Model>)this.PersonsList.DataSource;
+            this.ReloadData();
+            if(((BindingList<Server.Model>)this.PersonsList.DataSource).Count == 0)
+            {
+                this.RecordsFrom -= Form1.DATA_SLICE;
+                //rollback, because we probably went too far
+                this.PersonsList.DataSource = personsList;
+            }
         }
     }
 }
